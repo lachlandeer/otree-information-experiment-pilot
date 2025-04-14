7adcb2b200d26e49fc78f39097f9d43421c47738
Lachlan Deer <lachlan.deer@gmail.com>
2025-04-14 11:58:31 +0200

    move away from live game to survey

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=0.005, participatio
 SESSION_CONFIGS = [dict(name='my_session', 
                         num_demo_participants=3, 
                         app_sequence=[
-                         'Introduction', 
-                         'asset_indiv_no_game', 
+                         #'Introduction', 
+                         #'asset_indiv_no_game', 
                          'GroupPreferenceElicitation', 
-                         #'asset_live_game', 
+                         'asset_indiv_no_game_duplicate'
                          #'bonus', 
-                         'CollectivismSurvey', 
-                         'DemographicsSurvey',
-                         'RandomPaymentResults'
+                         #'CollectivismSurvey', 
+                         #'DemographicsSurvey',
+                         #'RandomPaymentResults'
                          ]
                        )
                   ]
